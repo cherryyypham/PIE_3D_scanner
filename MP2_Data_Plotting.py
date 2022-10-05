b2ef0cfcb9298ea654f5d1dc8830d6ba8fff5466
Cherry Pham <meokkmun@gmail.com>
2022-10-04 20:00:37 -0400

    Staging commit before commit further changes

--- MP2_Data_Plotting.py
+++ MP2_Data_Plotting.py
@@ -1,14 +1,60 @@
+import csv
 import math
 import numpy as np
-from mpl_toolkits import mplot3d
 import matplotlib.pyplot as plt
-from mpl_toolkits.mplot3d import Axes3D
-from MP2_Data_Processing import z_arr, y_arr, x_arr
+
+arr = []
+
+pan_arr = []
+val_arr = []
+tilt_arr = []
+x_arr = []
+y_arr = []
+z_arr = []
+# string1 = ""
+
+with open("data3.csv") as f:
+    r2 = csv.reader(f)
+    for row in r2:
+        arr.append(row)
+
+for i in arr:
+    # string1 += str(i.index(j))
+    pan_arr.append(i[0])
+    val_arr.append(i[1])
+    tilt_arr.append(i[2])
+# print(string1)
+# x_arr = x_arr[:958]
+# y_arr = y_arr[:958]
+# z_arr = z_arr[:958]
+
+# print(arr)
+
+
+# for pan in pan_arr:
+#     pan = math.radians(int(pan))
+
+# for tilt in tilt_arr:
+#     tilt = math.radians(int(tilt))
+
+# for val in val_arr:
+#     val = (414091/(int(val) + 102.6) ** (1.548))
+
+for i in range(len(pan_arr)):
+    # converting polar coordinate data from arduino to cartesian
+    x = (414091/(int(val_arr[i]) + 102.6) ** (1.548)) * np.sin(math.radians(int(tilt_arr[i]))) * np.cos(math.radians(int(pan_arr[i])))
+    y = (414091/(int(val_arr[i]) + 102.6) ** (1.548))  * np.sin(math.radians(int(tilt_arr[i]))) * np.sin(math.radians(int(pan_arr[i])))
+    z = (414091/(int(val_arr[i]) + 102.6) ** (1.548)) * np.cos(math.radians(int(tilt_arr[i])))
+
+    x_arr.append(x)
+    y_arr.append(y)
+    z_arr.append(z)
 
 fig = plt.figure()
 ax = plt.axes(projection='3d')
+
 ax.scatter(z_arr, y_arr, x_arr, c=x_arr, cmap='viridis', linewidth=0.5)
 
-ax.set_title('Test Scan of Letter "M"')
+ax.set_title('Scan of Letter "M"')
 
 plt.show()
--- MP2_Data_Processing.py
+++ MP2_Data_Processing.py
@@ -1,37 +1,43 @@
-import math
-import random
-import numpy as np
-from mpl_toolkits import mplot3d
-import matplotlib.pyplot as plt
-from mpl_toolkits.mplot3d import Axes3D
-
 import serial
+import csv
 
 arduinoComPort = "/dev/ttyACM0"
 baudRate = 9600
 serialPort = serial.Serial(arduinoComPort, baudRate, timeout=1)
 
-while True:
+pan_arr = []
+val_arr = []
+tilt_arr = []
+
+take_data = True
+
+while take_data:
   lineOfData = serialPort.readline().decode()
 
   if len(lineOfData) > 0:
-    x, y, z = (int(float(i)) for i in lineOfData.split(','))
-    print("z = " + str(x), end="")
-    print(", y = " + str(y), end="")
-    print(", z = " + str(z))
-    fig = plt.figure()
-    ax = plt.axes(projection='3d')
-    
-    xo = list(range(0, 100))
-    yo = list(range(0, 100))
-    zo = list(range(0, 100))
+    if "Done" in lineOfData:
+      take_data = False
+    else:
+      pan, val, tilt = (int(float(i)) for i in lineOfData.split(','))
 
-    # random.shuffle(xo)
-    # random.shuffle(yo)
-    # random.shuffle(zo)
+      # pan = math.radians(pan)
+      # tilt = math.radians(tilt)
+      # val = (414091/(val + 102.6) ** (1.548))
 
-    ax.scatter(zo, yo, xo, c=xo, cmap='viridis', linewidth=0.5)
+      # converting polar coordinate data from arduino to cartesian
+      # x = val * np.sin(tilt) * np.cos(pan)
+      # y = val * np.sin(tilt) * np.sin(pan)
+      # z = val * np.cos(tilt)
 
-    ax.set_title('Test Scan of Letter "M"')
+      pan_arr.append(pan)
+      val_arr.append(val)
+      tilt_arr.append(tilt)
 
-    plt.show()
+      print("z = " + str(pan), end="")
+      print(", y = " + str(val), end="")
+      print(", z = " + str(tilt))
+
+with open("data3.csv", "w", newline = "") as f:
+  writer = csv.writer(f)
+  for i in range(len(pan_arr)):
+    writer.writerow([pan_arr[i], val_arr[i], tilt_arr[i]])
